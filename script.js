3f04b150c2d77c64f2378253af4c550cea4396d3
Yaroslavna <pro003@inbox.ru>
2021-01-06 16:51:26 +0300

    first task Ivan

--- script.js
+++ script.js
@@ -1,35 +1,22 @@
 'use strict';
-
-let money,
-    time,
-    result,
-    expensesOne,
-    expensesTwo;
-
-money = +prompt("Ваш бюджет на месяц? ", "10");
-time = prompt("Введите дату в формате YYYY-MM-DD", "");
-expensesOne = prompt("Введите обязательную статью расходов в этом месяце", "");
-expensesTwo =prompt("Во сколько обойдется?", ""); 
-
-let optionalExpenses = {
-
-}
-
-let expenses = {
-    first : expensesOne,
-    second : expensesTwo
-}
-
-let income = [
-
-]
+let money = prompt("Ваш бюджет на месяц?", ''),
+	time = prompt('Введите дату в формате YYYY-MM-DD', '');
 
 let appData = {
-    budget : money,
-    timeData : time,
-    obras : expenses,
-    neobras : optionalExpenses,
-    inCome : income
-}
-result = appData.budget / 30;
-alert(result);
+	budget: money,
+	expenses: {},
+	optionalExpenses: {},
+	income: [],
+	timeData: time,
+	savings: false
+};
+
+let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a2 = prompt("Во сколько обойдется?", ''),
+	a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a4 = prompt("Во сколько обойдется?", '');
+
+appData.expenses.a1 = a2;
+appData.expenses.a3 = a4;
+
+alert(appData.budget / 30);
